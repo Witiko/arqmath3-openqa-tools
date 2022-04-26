a5bb9f57ff4891f2e327459b4340c9eb8d8c8ff9
Vít Novotný <witiko@mail.muni.cz>
2022-04-26 14:27:57 +0200

    Warn when dollar signs are imbalanced

--- scripts/validate_task3_results.py
+++ scripts/validate_task3_results.py
@@ -1,6 +1,9 @@
 import argparse
-from collections import defaultdict
+import logging
 import csv
+
+
+LOGGER = logging.getLogger(__name__)
 
 
 def read_task3_result_file(file_path, max_answer_length=1200):
@@ -9,13 +12,17 @@ def read_task3_result_file(file_path, max_answer_length=1200):
     @param file_path: file path to input file
     @return: dict of topic ids and results
     """
-    result = defaultdict(lambda: dict())
+    result = dict()
     with open(file_path, newline='') as csv_file:
         csv_reader = csv.reader(csv_file, delimiter='\t')
         for line_number, row in enumerate(csv_reader):
+            line_number += 1
             topic_id, rank, score, run_id, answer = row
+            if answer.count('$') % 2 == 1:
+                LOGGER.warning(f'An odd number of dollar signs ($) in answer to topic {topic_id} '
+                               f'on line {line_number}. This may indicate invalid TeX code.')
             if len(answer) > max_answer_length:
-                raise ValueError(f'Answer to topic {topic_id} on line {line_number + 1} contains '
+                raise ValueError(f'Answer to topic {topic_id} on line {line_number} contains '
                                  f'{len(answer)} Unicode characters, but at most '
                                  f'{max_answer_length} were expected')
             result[topic_id] = (topic_id, str(rank), str(score), run_id, answer)
@@ -27,6 +34,8 @@ def main():
     example: python3 validate_task3_results.py -in "TangentS_task3_2021.tsv"
     @return:
     """
+    logging.basicConfig(level=logging.INFO, format='%(levelname)s: %(message)s')
+
     parser = argparse.ArgumentParser(description='Convert results from ARQMath Task 3')
 
     parser.add_argument('-in', help='Input result file in ARQMath format for ARQMath Task 3', required=True)
@@ -35,7 +44,7 @@ def main():
 
     input_file = args['in']
     _ = read_task3_result_file(input_file)
-    print(f'{input_file} validates')
+    LOGGER.info(f'{input_file} validates')
 
 
 if __name__ == "__main__":
