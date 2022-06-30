5365e145061a46510f7d943f68ec1a6a0243b719
Vít Novotný <witiko@mail.muni.cz>
2022-06-30 03:30:06 +0200

    Add `Dockerfile`

---
+++ launch-docker.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+# Builds a Docker image with SCM and Jupyter Lab and runs the image at apollo.fi.muni.cz
+set -e -o xtrace
+
+GPUS=16
+IMAGE_NAME=witiko/arqmath3-openqa-tools:latest
+
+DOCKER_BUILDKIT=1 docker build --build-arg UID="$(id -u)" --build-arg GID="$(id -g)" --build-arg UNAME="$(id -u -n)" . -t "$IMAGE_NAME"
+docker run --rm -it -u "$(id -u):$(id -g)" --runtime=nvidia -e CUDA_DEVICE_ORDER=PCI_BUS_ID -e NVIDIA_VISIBLE_DEVICES="$GPUS" -v "$PWD"/..:/workdir:rw -w /workdir "$IMAGE_NAME" bash
